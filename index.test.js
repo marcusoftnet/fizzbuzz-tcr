d5158eb16b47d7beb7c818673d3e3d3a02b11273
Marcus Hammarberg <marcusoft.net@gmail.com>
2019-01-11 14:08:03 +0100

    TCR @ Fri 11 Jan 14:08:03 2019

--- index.test.js
+++ index.test.js
@@ -54,7 +54,7 @@ describe('FizzBuzz string', () => {
   it('returns "1, 2, Fizz" for "1,2,3"', () => {
     assert.equal(fizzBuzzer.string('1, 2, 3'), '1, 2, Fizz')
   })
-  it('returns "1, 2, Fizz" for "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15"', () => {
+  it('the whole kata', () => {
     assert.equal(fizzBuzzer.string('1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15'), '1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz')
   })
 })
